321ae15b6a739fe5e76bcf4f16c90865a74552da
Patrycjusz Naklicki <naklicki.patryk@gmail.com>
2016-10-08 19:13:45 +0200

    Added project files

---
+++ 01_StudNet/01_SourceCode/Studnet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Studnet.Startup))]
+namespace Studnet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
