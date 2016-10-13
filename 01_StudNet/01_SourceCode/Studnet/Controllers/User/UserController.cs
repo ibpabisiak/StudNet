d2d6bc488905641651e6b79952db89aa1774034b
Mateusz Nowak <msnowak94@gmail.com>
2016-10-13 09:31:45 +0200

    Dodano wysyłanie maila aktywacyjnego
    mail aktywacyjny, usunięto stary plik z bazą danych, dodano view z
    informacją po wysłaniu formularza rejestracyjnego.

--- 01_StudNet/01_SourceCode/Studnet/Controllers/User/UserController.cs
+++ 01_StudNet/01_SourceCode/Studnet/Controllers/User/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +26,33 @@ namespace Studnet.Controllers.User
         {
             AppData.Instance().studnetDatabase.users.Add(user);
             AppData.Instance().studnetDatabase.SaveChanges();
+
+            // send verification mail
+
+            UrlHelper urlHelper = new UrlHelper(this.ControllerContext.RequestContext);
+            string url = urlHelper.Action("VerifyMail", "User", new {mail = user.user_mail});
+            MailMessage mail = new MailMessage("studnet@msnowak.webd.pl", user.user_mail);
+            mail.Subject = "Weryfikacja adresu email";
+            mail.Body = "Witamy,<br>" +
+                        "Aby zweryfikować adres e-mail wejdź pod adres <a href=\"http://address.com"+url+"\">KLIK</a>" + 
+                        "<br>Pozdrawiamy,<br>"+
+                        "Zespół StudNet";
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Port = 25;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Host = "msnowak.webd.pl";
+            smtpClient.Credentials = new NetworkCredential("studnet@msnowak.webd.pl", "studnet");
+            smtpClient.Send(mail);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult VerifyMail(string userMail)
+        {
+            //tutaj będzie weryfikacja adresu email
+            string returnMsg = "e-mail " + userMail + " został zweryfikowany";
+            ViewBag.Message = returnMsg;
             return View();
         }
     }
