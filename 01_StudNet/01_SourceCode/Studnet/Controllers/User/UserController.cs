ae5d9423a62d53b2c0a856743786d75e06ab5b70
Mateusz Nowak <msnowak94@gmail.com>
2016-10-14 22:05:48 +0200

    Login form

--- 01_StudNet/01_SourceCode/Studnet/Controllers/User/UserController.cs
+++ 01_StudNet/01_SourceCode/Studnet/Controllers/User/UserController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using Studnet.Models;
 
 namespace Studnet.Controllers.User
 {
@@ -25,13 +26,14 @@ namespace Studnet.Controllers.User
         [HttpPost]
         public ActionResult Register(Models.User user)
         {
+
             AppData.Instance().studnetDatabase.users.Add(user);
             AppData.Instance().studnetDatabase.SaveChanges();
 
             // send verification mail
 
             UrlHelper urlHelper = new UrlHelper(this.ControllerContext.RequestContext);
-            string url = urlHelper.Action("VerifyMail", "User", new {mail = user.user_mail});
+            string url = "http://" + Request.Url.Host + urlHelper.Action("VerifyMail", "User", new {mail = Server.HtmlDecode(user.user_mail) });
             MailMessage mail = new MailMessage("studnet@msnowak.webd.pl", user.user_mail);
             mail.Subject = "Weryfikacja adresu email";
             mail.Body = "Witamy,<br>" +
@@ -74,5 +76,11 @@ namespace Studnet.Controllers.User
             ViewBag.Message = returnMsg;
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
